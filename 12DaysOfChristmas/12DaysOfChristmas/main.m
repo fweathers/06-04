645a98cd1b01230152529940c36ae4f4e07a86d5
Felicia Weathers <fel.weathers@gmail.com>
2015-06-06 11:10:15 -0400

    First commit

---
+++ 12DaysOfChristmas/12DaysOfChristmas/main.m
@@ -0,0 +1,34 @@
+//
+//  main.m
+//  12DaysOfChristmas
+//
+//  Created by Felicia Weathers on 6/4/15.
+//  Copyright (c) 2015 Felicia Weathers. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+
+        NSArray *days = @[@"first", @"second", @"third", @"fourth", @"fifth", @"sixth", @"seventh", @"eighth", @"ninth", @"tenth", @"eleventh", @"twelfth"];
+                
+        NSArray *gifts = @[@"A Partridge in a pear tree", @"Two turtle doves", @"Three French hens", @"Four calling birds", @"Five gold rings", @"six geese a-laying", @"seven swans a-swimming", @"eight maids a-milking", @"nine ladies dancing", @"ten lords a-leaping", @"eleven pipers piping", @"twelve drummers drumming"];
+        
+        NSLog(@"On the %@ day of Christmas my true love sent to me, %@", days [0], gifts[0]);
+        NSLog(@"On the %@ day of Christmas my true love sent to me, %@, and %@", days [1], gifts[1], gifts[0]);
+        NSLog(@"On the %@ day of Christmas my true love sent to me, %@, %@, and %@", days [2], gifts[2],gifts[1], gifts[0]);
+        NSLog(@"On the %@ day of Christmas my true love sent to me, %@, %@, %@, and %@", days [3], gifts[3],gifts[2], gifts[1], gifts[0]);
+        NSLog(@"On the %@ day of Christmas my true love sent to me, %@, %@, %@, %@, and %@", days [4], gifts[4],gifts[3], gifts[2], gifts[1], gifts[0]);
+        NSLog(@"On the %@ day of Christmas my true love sent to me, %@, %@, %@, %@, %@, and %@", days [5], gifts[5], gifts[4], gifts[3], gifts[2], gifts[1], gifts[0]);
+        NSLog(@"On the %@ day of Christmas my true love sent to me, %@, %@, %@, %@, %@, %@, and %@", days [6], gifts[6], gifts[5], gifts[4], gifts[3], gifts[2], gifts[1], gifts[0]);
+        NSLog(@"On the %@ day of Christmas my true love sent to me, %@, %@, %@, %@, %@, %@, %@, and %@", days [7], gifts[7], gifts[6], gifts[5], gifts[4], gifts[3], gifts[2], gifts[1], gifts[0]);
+        NSLog(@"On the %@ day of Christmas my true love sent to me, %@, %@, %@, %@, %@, %@, %@, %@, and %@", days [8], gifts[8], gifts[7], gifts[6], gifts[5], gifts[4], gifts[3], gifts[2], gifts[1], gifts[0]);
+        NSLog(@"On the %@ day of Christmas my true love sent to me, %@, %@, %@, %@, %@, %@, %@, %@, %@, and %@", days [9], gifts[9], gifts[8], gifts[7], gifts[6], gifts[5], gifts[4], gifts[3], gifts[2], gifts[1], gifts[0]);
+        NSLog(@"On the %@ day of Christmas my true love sent to me, %@, %@, %@, %@, %@, %@, %@, %@, %@, %@, and %@", days [10], gifts[10], gifts[9], gifts[8], gifts[7], gifts[6], gifts[5], gifts[4], gifts[3], gifts[2], gifts[1], gifts[0]);
+        NSLog(@"On the %@ day of Christmas my true love sent to me, %@, %@, %@, %@, %@, %@, %@, %@, %@, %@, %@, and %@", days [11], gifts[11], gifts[10], gifts[9], gifts[8], gifts[7], gifts[6], gifts[5], gifts[4], gifts[3], gifts[2], gifts[1], gifts[0]);
+
+    return 0;
+
+    }
+}
---
+++ variables2/variables2/main.m
@@ -0,0 +1,72 @@
+//
+//  main.m
+//  variables2
+//
+//  Created by Felicia Weathers on 6/4/15.
+//  Copyright (c) 2015 Felicia Weathers. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+//        NSLog(@"Hello, World!");
+//        
+//        char g = 'd';
+//        char b = 'g';
+//        char d = b + g;
+//        NSLog(@"g=%c, b=%c, d=%c", g, b, d);
+//        NSLog(@"g=%i, b=%i, d=%i", g, b, d);
+        
+//        int a = 5;
+//        int b = 6;
+//        int c = 7;
+//        int d = 5;
+//        int result;
+//
+        // 0 is false / no ; 1 is yes / true
+//        BOOL answer = b < d;
+//        NSLog(@"BOOL answer = b < d, %d", result);
+//        
+
+//        int a = 26;
+//        CGFloat b = 33.3;
+//        NSString *z = @"zzzz";
+//        NSString *y = @"yyyy";
+//        NSLog(@"Value: %i", a ==32);
+//        NSLog(@"Value: %i", 6 <= 7);
+//
+//          char g = 'd';
+//          int a = 5;
+//          int result;
+//        NSLog(@"g=%c", g);
+//        NSLog(@"g=%i", g);
+//        NSLog(@"integer Value of d = %i", g);
+//        
+//        NSLog(@"Value: %i", g==5);
+//        NSLog(@"Value: %i", a==g);
+//
+        
+//        char b = 'g';
+//        char a = '$';
+//        char g = b + a;
+//        NSLog(@"b=%c, a=%c, g=%c", b, a, g);
+//        NSLog(@"b=%i, a=%i, g=%i", b, a, g);
+//        
+        
+        NSLog(@"+--------------------------------------------------+");
+        
+        NSArray *classes = [@"English III", @"Precalculus", @"Music Theory", @"Biotechnology", @"Principls of Technology I", @"Latin II", @"AP US History", "Busines Computer Informatino Systems"];
+        
+        NSArray *teachers = [@"Ms. Lapan", @"Mrs. Gideon", @"Mr. Davis", @"Ms. Palmer", "@Ms. Garcia", @"Mrs.Barnett", @"Ms. Johannessen", @"Mr. James"];
+        for(int i=0; i<[classes count]; i++ {
+            NSLog(@"%d:)
+        }
+        
+        
+        
+        
+    }
+    return 0;
+}
